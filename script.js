9507525f0ca51e19dcdfa78a2deba49944992408
unknown <valen1984@gmail.com>
2022-06-29 17:32:40 -0300

    MASTER: Creacion de index.html

---
+++ script.js
@@ -0,0 +1,17 @@
+window.onload = function() {
+const asd = [
+                [0,1,1,3,4], 
+                [0,1,2,3,4], 
+                [0,1,3,3,4], 
+                [0,1,4,3,4], 
+                [0,1,5,3,4], 
+                [0,1,6,3,4]
+            ];
+
+for (let i = 0; i < asd.length; i++) {
+    console.log (asd[i])
+    for (let x = 0; x < asd.length; x++) {
+        console.log (asd[i][x]); 
+    }
+}
+}
