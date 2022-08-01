99f39bee48806f504d13199a093e1e3a765ccb84
unknown <valen1984@gmail.com>
2022-07-31 22:57:11 -0300

    MASTER: Fn Cargar Partida
    Description: Reloj no funciona

--- scripts.js
+++ scripts.js
@@ -109,6 +109,193 @@ function GuardarProgreso(){
     window.location.href = "../index.html";
 }
 
+const loadGame = function(indice){
+    gameOver = false;
+    let modal = document.getElementById("modalPartidas");
+    modal.style.display = "none";
+
+    for (let indice = 0; indice < 6; indice++){
+        let fieldset = document.getElementById(`fila${indice}`);
+        fieldset.disabled=false;
+    }
+
+    // Traigo del localStorage el array "saves"
+    let savesArray = JSON.parse(localStorage.getItem('saves'));
+
+    let actualArray = savesArray[indice].respuestas;
+    let actualPalabra = savesArray[indice].palabraGanadora;
+    let actualTiempo = savesArray[indice].tiempo
+    let actualUsuario = savesArray[indice].usuario;
+    let actualColorTablero = savesArray[indice].colorTablero;
+
+    colorTablero = actualColorTablero;
+
+    //guardarRespuestaPartidaCargada(indice)
+
+    pintarTablero();
+
+    palabraGanadora = actualPalabra;
+    document.querySelector("#time").innerHTML = actualTiempo;
+    document.getElementById("nombre-jugador-input").value = actualUsuario;
+
+
+    //Escribo en valores obtenidos en input
+    for (let iFila = 0; iFila < 6; iFila++) {
+        for (let iCol=0; iCol<5; iCol++){
+            let input = document.getElementById(`f${iFila}c${iCol}`);
+            if(actualArray[iFila][iCol] !== undefined){
+            input.value = actualArray[iFila][iCol];
+            }
+        }
+    }
+
+    console.log(actualPalabra);
+    console.log(actualTiempo);
+    console.log(actualUsuario);
+
+    hideBtn();
+    mensajeDeErrorValor();
+
+    //Realcular tiempo
+    let sec = actualTiempo.slice(3);
+    let min = actualTiempo.slice(0, 2);
+    let secTransform = Math.round((sec/60) * 100);
+    let calculoTiempo = Math.round(((min + secTransform) /100) * 60);
+    var timer = calculoTiempo;
+    display = document.querySelector("#time");
+    startTimer(timer, display);
+
+
+    //misma funcion pero al cargar partidas (con palabra actual)
+    function guardarRespuestaPartidaCargada(indice){
+        for (let iCol = 0; iCol < 5; iCol++){
+            let input = document.getElementById(`f${indice}c${iCol}`).value;
+            respuestas[indice].push(input);
+        }
+        revisarResultadoPartidaCargada(respuestas[indice], indice);
+    }
+
+    function revisarResultadoPartidaCargada(respuesta, indice){
+        respuesta.forEach(function(elemento, index){
+            if(elemento === arrayActualPalabra[index]){
+                colorTablero[indice][index] = colores.VERDE;
+            }
+            else if(arrayActualPalabra.includes(elemento)){
+                colorTablero[indice][index] = colores.AMARILLO;
+            }
+            else if(!arrayActualPalabra.includes(elemento)){
+                colorTablero[indice][index] = colores.GRIS;
+            }
+        })
+        pintarTablero();
+    }
+
+    arrayActualPalabra = actualPalabra.split("");
+
+
+    //Guarla la respuesta de la partida solo de las filas  guardadas
+    for (let indice = 0; indice < 6; indice++){
+        let fieldset = document.getElementById(`fila${indice}`);
+        let validarCaracter = document.querySelectorAll(`#fila${indice} input`);
+
+        let valor0 = validarCaracter[0].value;
+        let valor1 = validarCaracter[1].value;
+        let valor2 = validarCaracter[2].value;
+        let valor3 = validarCaracter[3].value;
+        let valor4 = validarCaracter[4].value;
+
+        //condicional para que no impacte guardarRespuestaPartidaCargada en espacios vacios
+        if (valor0 !== "" && valor1 !== "" && valor2 !== "" && valor3 !== ""&& valor4 !== ""){
+            guardarRespuestaPartidaCargada(indice);
+            fieldset.disabled=true;
+        }
+        if (valor0 == ""){
+            validarCaracter[0].focus();
+            break //se posiciona en la primer fila vacia
+        }
+    }
+
+    //Funcion inicio() modificada para partida guardada
+    for (let indice = 0; indice < 6; indice++){
+        let fieldset = document.getElementById(`fila${indice}`);
+        fieldset.onkeydown = function (event){//el resto del juego guardarRespuesta con enter
+            if(event.key === `Enter`){
+                let validarCaracter = document.querySelectorAll(`#fila${indice} input`);
+                var regex = new RegExp ("[A-Z]");
+                let valor0 = validarCaracter[0].value;
+                let valor1 = validarCaracter[1].value;
+                let valor2 = validarCaracter[2].value;
+                let valor3 = validarCaracter[3].value;
+                let valor4 = validarCaracter[4].value;
+
+                let input0 = regex.test(valor0);
+                let input1 = regex.test(valor1);
+                let input2 = regex.test(valor2);
+                let input3 = regex.test(valor3);
+                let input4 = regex.test(valor4);
+
+                if (valor0 == "" || valor1 == "" || valor2 == "" || valor3 == "" || valor4 == ""){
+                    mensajeDeErrorEnter();
+                }
+                else if (input0 == false || input1 == false || input2 == false || input3 == false || input4 == false){
+                    mensajeDeErrorValor();
+                }
+                else if (valor0.length > 1 || valor1.length  > 1 || valor2.length > 1 || valor3.length > 1 || valor4.length > 1 ){
+                    mensajeDeErrorUnaLetra();
+                }
+                else{
+                guardarRespuestaPartidaCargada(indice);
+                eliminarMensajeDeError();
+
+                let respuestaUsuario = respuestas[indice];
+                let respuestaUsuarioString = respuestaUsuario.join("");
+
+                if (respuestaUsuarioString == palabraGanadora){
+                    gameOver = true;
+                    showBtn();
+                    document.getElementById("mensaje-resultado").style.color = "rgb(21, 211, 21)";
+                    document.getElementById("mensaje-resultado").innerHTML = "--- GANASTE!! --- ";
+                    scorePartidaGanada(indice); // Guardamos los datos de la partida con el score
+                    bloqueoFieldsetGanarOPerder();
+                }
+
+                if (indice == 0 && respuestaUsuarioString != palabraGanadora){
+                    document.getElementById("fila1").disabled=false;
+                    document.getElementById("fila0").disabled=true;
+                    document.getElementById("f1c0").focus();
+                }
+                if (indice == 1 && respuestaUsuarioString != palabraGanadora){
+                    document.getElementById("fila2").disabled=false;
+                    document.getElementById("fila1").disabled=true;
+                    document.getElementById("f2c0").focus();
+                }
+                if (indice == 2 && respuestaUsuarioString != palabraGanadora){
+                    document.getElementById("fila3").disabled=false;
+                    document.getElementById("fila2").disabled=true;
+                    document.getElementById("f3c0").focus();
+                }
+                if (indice == 3 && respuestaUsuarioString != palabraGanadora){
+                    document.getElementById("fila4").disabled=false;
+                    document.getElementById("fila3").disabled=true;
+                    document.getElementById("f4c0").focus();
+                }
+                if (indice == 4 && respuestaUsuarioString != palabraGanadora){
+                    document.getElementById("fila5").disabled=false;
+                    document.getElementById("fila4").disabled=true;
+                    document.getElementById("f5c0").focus();
+                }
+                if (indice == 5  && respuestaUsuarioString != palabraGanadora){
+                    gameOver = true;
+                    showBtn();
+                    document.getElementById("mensaje-resultado").innerHTML = `Game OVER! No quedan mas intentos. La palabra es: "${palabraGanadora}"`;
+                    bloqueoFieldsetGanarOPerder();
+                    }
+                }
+            }
+        }
+    }
+}
+
 //Funcion para asignar score, se ejecuta cuando el jugador gana
 function scorePartidaGanada(fila){
 
@@ -391,7 +578,14 @@ var palabraGanadora = elegirPalabraAlAzar(palabrasDisponibles);
 
 var arrayPalabraGanadora = palabraGanadora.split("");
 
-
+// Nueva partida, esconder botones
+function hideBtn() {
+    document.getElementById("nueva-partida").style.display="none";
+    document.getElementById("cargar-partida").style.display="none";
+    document.getElementById("guardar-partida").style.display="inline-block";
+    document.getElementById("timer").style.display="block";
+    document.getElementById("time").style.display="inline";
+    }
 
 function showBtn() {
     document.getElementById("volver-a-jugar-partida").style.display="inline-block";
@@ -421,160 +615,111 @@ function tabular(e) {
     }
 }
 
-    window.onload = function(){
+window.onload = function(){
+    console.log(palabraGanadora)
 
-        let inputsForm = document.getElementById("grilla").querySelectorAll("input");
-    
-        inputsForm.forEach(function (x){
-            x.addEventListener("keyup", tabular);
-        })
-    
-        //Funcion para ingresar nombre
-    
-        const nuevaPartida = document.getElementById("nueva-partida");
-        const volverAJugar = document.getElementById("volver-a-jugar-partida");
-        const gurdarPartida = document.getElementById("guardar-partida");
-        const rankingPartida = document.getElementById("ranking-partida");
-        const cargarPartida = document.getElementById("cargar-partida");
-    
-        const form = document.getElementById("formulario-usuario");
-        const name = document.getElementById("nombre-jugador-input");
-    
-        const ordenFecha = document.getElementById("orden-por-fecha");
-        const ordenPuntaje = document.getElementById("orden-por-puntaje");
-    
-        form.addEventListener("submit", function(e){
-            e.preventDefault();
-    
-            var regexName = new RegExp ("^[A-Za-z]+$");
-            let nameValue = name.value;
-            let regexValue = regexName.test(nameValue);
-    
-            if (name.value.length < 3 || regexValue == false || name.value == "") {
-                errorNombre.innerHTML = "Ingrese un nombre mayor a 2 digitos. Solo letras sin espacios."
-                return false; //se utiliza para abortar la funcion
-            } else {
-             document.getElementById("nombre-jugador").style.display="none";
-                estadoGanador = false;
-                inicio();
-                pintarTablero();
-                timer();
-                hideBtn();
-                document.getElementById("fila0").disabled=false;
-                document.getElementById("f0c0").focus();
-                mensajeDeErrorValor();
-            }
-        })
-    
-        nuevaPartida.addEventListener("click", function(){
-            document.getElementById("nombre-jugador").style.display="flex";
-            document.getElementById("nombre-jugador-input").focus();
-        })
-    
-        volverAJugar.addEventListener("click", function(){
-            estadoGanador = false;
-            estadoPerdedor = false;
-            location.reload();
-        })
-    
-        gurdarPartida.addEventListener("click", function(){
-            GuardarProgreso();
-        })
-    
-        rankingPartida.addEventListener("click", function(){
-            obtenerPuntajes();
-            mostrarModal();
-        })
-    
-        ordenFecha.addEventListener("click", function(){
-            obtenerPuntajes()
-        })
-    
-        ordenPuntaje.addEventListener("click", function(){
-            orderscoreboard()
-        })
-    
-        // Nueva partida, esconder botones
-        function hideBtn() {
-            document.getElementById("nueva-partida").style.display="none";
-            document.getElementById("cargar-partida").style.display="none";
-            document.getElementById("guardar-partida").style.display="inline-block";
-            document.getElementById("timer").style.display="block";
-            document.getElementById("time").style.display="inline";
-            }
-    
-        function timer() {
-            var fiveMinutes = 60 * 5,
-            display = document.querySelector("#time");
-            startTimer(fiveMinutes, display);
-        }
-        
-        var estadoGanador = false;
-        var estadoPerdedor = false;
+    let inputsForm = document.getElementById("grilla").querySelectorAll("input");
 
-        function startTimer(duration, display) {
-            var timer = duration, minutes, seconds;
-            var reloj = setInterval(function () {
-                minutes = parseInt(timer / 60, 10);
-                seconds = parseInt(timer % 60, 10);
-    
-                minutes = minutes < 10 ? "0" + minutes : minutes;
-                seconds = seconds < 10 ? "0" + seconds : seconds;
-    
-                display.textContent = minutes + ":" + seconds;
-    
-                if (estadoGanador || estadoPerdedor){
-                    clearInterval(reloj);
-                }
-    
-                if (timer < 60 * 3){
-                    document.getElementById("time").style.color="rgb(226, 226, 85)";
-                }
-    
-                if (timer < 60){
-                    document.getElementById("time").style.color="rgb(226, 38, 38)";
-                }
-    
-                if (--timer < 0) {
-                    estadoPerdedor = true;
-                    timer = duration;
-                    showBtn();
-                    document.getElementById("mensaje-resultado").innerHTML = `Game OVER! Tiempo finalizado. La palabra es: ${palabraGanadora}`;
-                    bloqueoFieldsetGanarOPerder();
-                }
-            }, 1000);
-        }
-    
-        cargarPartida.addEventListener("click", function(){
+    inputsForm.forEach(function (x){
+        x.addEventListener("keyup", tabular);
+    })
+
+    //Funcion para ingresar nombre
+
+    const nuevaPartida = document.getElementById("nueva-partida");
+    const volverAJugar = document.getElementById("volver-a-jugar-partida");
+    const gurdarPartida = document.getElementById("guardar-partida");
+    const rankingPartida = document.getElementById("ranking-partida");
+    const cargarPartida = document.getElementById("cargar-partida");
+
+    const form = document.getElementById("formulario-usuario");
+    const name = document.getElementById("nombre-jugador-input");
+
+    const ordenFecha = document.getElementById("orden-por-fecha");
+    const ordenPuntaje = document.getElementById("orden-por-puntaje");
+    const numeroPartida = document.getElementById("numero-partida");
+
+    form.addEventListener("submit", function(e){
+        e.preventDefault();
+
+        var regexName = new RegExp ("^[A-Za-z]+$");
+        let nameValue = name.value;
+        let regexValue = regexName.test(nameValue);
+
+        if (name.value.length < 3 || regexValue == false || name.value == "") {
+            errorNombre.innerHTML = "Ingrese un nombre mayor a 2 digitos. Solo letras sin espacios."
+            return false; //se utiliza para abortar la funcion
+        } else {
             document.getElementById("nombre-jugador").style.display="none";
-            ordenPuntaje.style.display="none"
-            numeroPartida.style.display="table-cell"
-            obtenerSaves();
-            mostrarModal();
-    
-            ordenFecha.addEventListener("click", function(){
-                obtenerSaves();
-            })
+            gameOver = false;
+            inicio();
+            timer();
+            hideBtn();
+            document.getElementById("fila0").disabled=false;
+            document.getElementById("f0c0").focus();
+            mensajeDeErrorValor();
+        }
+    })
+
+    nuevaPartida.addEventListener("click", function(){
+        document.getElementById("nombre-jugador").style.display="flex";
+        name.focus();
+    })
+
+    volverAJugar.addEventListener("click", function(){
+        gameOver = false;
+        location.reload();
+    })
+
+    gurdarPartida.addEventListener("click", function(){
+        saveProgress();
+    })
+
+    rankingPartida.addEventListener("click", function(){
+        ordenPuntaje.style.display="table-cell"
+        numeroPartida.style.display="none"
+        obtenerPuntajes();
+        mostrarModal();
+
+        ordenFecha.addEventListener("click", function(){
+            obtenerPuntajes();
         })
-    
-        function mostrarModal() {
-            // Ejecuto modal -----------------------------------------------------------
-            let modal = document.getElementById("modalPartidas");
-            let span = document.getElementById("close");
-    
-            // Lo hago visible
-            modal.style.display = "block";
-    
-            // Si clickea el "botón" de aceptar escondo el modal
-            span.onclick = function () {
+
+        ordenPuntaje.addEventListener("click", function(){
+            ordenalTablaPuntaje();
+        })
+    })
+
+    cargarPartida.addEventListener("click", function(){
+        document.getElementById("nombre-jugador").style.display="none";
+        ordenPuntaje.style.display="none"
+        numeroPartida.style.display="table-cell"
+        obtenerSaves();
+        mostrarModal();
+
+        ordenFecha.addEventListener("click", function(){
+            obtenerSaves();
+        })
+    })
+
+    function mostrarModal() {
+        // Ejecuto modal --------
+        let modal = document.getElementById("modalPartidas");
+        let span = document.getElementById("close");
+
+        // Lo hago visible
+        modal.style.display = "block";
+
+        // Si clickea el "botón" de aceptar escondo el modal
+        span.onclick = function () {
+            modal.style.display = "none";
+        }
+
+        // Si clickea fuera del modal, lo escondo
+        window.onclick = function (event) {
+            if (event.target == modal) {
                 modal.style.display = "none";
-            }
-    
-            // Si clickea fuera del modal, lo escondo
-            window.onclick = function (event) {
-                if (event.target == modal) {
-                    modal.style.display = "none";
-                }
             }
         }
     }
+}
